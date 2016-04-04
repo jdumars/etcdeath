3049bce33f63cca019747679bae8b24ce38d139f
Jason Singer DuMars <jdumars@deis.com>
2016-04-04 16:38:56 -0600

    added keyflood

---
+++ stress/keyflood.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+ARG=$1
+
+if [[ -z "$ARG" && -z "$VICTIM" ]] ; then
+	echo "You must specify an IP address or hostname, or have the VICTIM env var set"
+	echo "See https://github.com/jdumars/etcdeath for more documentation."
+	exit 0
+fi
+
+# Flood etcd keyspace
+
+# Determine if md5 or md5sum
+
+which md5sum
+if [[ $? -eq 1 ]] ; then
+	MD5=`which md5`
+else
+	MD5=`which md5sum`
+fi
+
+printf '\n'
+printf 'Key count to write to etcd [ infinite ] : '
+read -r COUNT
+
+if [[ -z "$COUNT" ]] ; then
+	
+	COUNT=1
+
+	while [[ $COUNT -lt 999999999999 ]] ; do
+
+		VAL=`echo $COUNT | $MD5`
+		curl -L -X PUT http://127.0.0.1:2379/v2/keys/$VAL -d value="$COUNT"
+		COUNT=$[COUNT+1]
+	done
+	exit 0
+
+else
+	INC=1
+	while [[ $COUNT -le $INC ]] ; do
+
+    VAL=`echo $COUNT | $MD5`
+    curl -L -X PUT http://127.0.0.1:2379/v2/keys/$VAL -d value="$COUNT"
+    COUNT=$[COUNT+1]
+  done
+  exit 0
+
+fi
+	
